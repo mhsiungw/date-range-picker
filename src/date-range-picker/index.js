d5964adbccf8eea86d3c16595451e86d01bb551c
min <minhsiungw@gmail.com>
2024-05-27 15:26:58 +0800

    add date-range-picker component

--- src/date-range-picker/index.js
+++ src/date-range-picker/index.js
@@ -1,3 +1,63 @@
-export default function DateRangePicker() {
-  return <div>DateRangePicker</div>;
+import { useMemo, useState, useEffect } from "react";
+import Day from "./components/day";
+import Week from "./components/week";
+import Header from "./components/header";
+import Utils from "./utils";
+import "./style.css";
+
+export default function DateRangePicker({ onChange }) {
+  const [range, setRange] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(Utils.getCurrentMonth());
+  const weeks = useMemo(() => Utils.getWeekArray(currentMonth), [currentMonth]);
+
+  useEffect(() => {
+    onChange(range);
+  }, [range]);
+
+  const handlePrevMonthSelect = () => {
+    setCurrentMonth(Utils.subtractMonth(currentMonth, 1));
+  };
+
+  const handleNextMonthSelect = () => {
+    setCurrentMonth(Utils.addMonth(currentMonth, 1));
+  };
+
+  const handleDaySelect = (nextDay) => {
+    setRange(([start, end]) => {
+      if (Utils.isSameOrBefore(nextDay, start)) {
+        return [nextDay, end];
+      }
+
+      if (Utils.isSameOrAfter(nextDay, end)) {
+        return [start, nextDay];
+      }
+
+      return [start, nextDay];
+    });
+  };
+
+  return (
+    <div className="date-range-picker">
+      <Header
+        header={currentMonth.format("YYYY 年 M 月")}
+        onPrev={handlePrevMonthSelect}
+        onNext={handleNextMonthSelect}
+      />
+      {weeks.map((week) => (
+        <Week key={`week-${week[0]}`}>
+          {week.map((day) => (
+            <span key={day.toString()} onDoubleClick={() => setRange([])}>
+              <Day
+                day={day}
+                isToday={Utils.isToday(day)}
+                isActive={Utils.isBetween(day, range)}
+                isCurrentMonth={Utils.isSameMonth(currentMonth, day)}
+                onDaySelect={handleDaySelect}
+              />
+            </span>
+          ))}
+        </Week>
+      ))}
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import DateRangePicker from "./date-range-picker";
+import "./style.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-const App = () => (
-  <div>
-    <DateRangePicker />
-  </div>
-);
+const App = () => {
+  const [range, setRange] = useState([]);
+
+  return (
+    <>
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <DateRangePicker onChange={setRange} />
+        <div style={{ width: "200px" }}>
+          <div>Start: {range[0]?.format("YYYY 年 MM 月 DD 號") || "--"}</div>
+          <div>End: {range[1]?.format("YYYY 年 MM 月 DD 號") || "--"}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 root.render(<App />);
